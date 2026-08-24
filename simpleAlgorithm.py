a10576350693775244c70a267e324284c1947989
farhankhan011 <farhank023102@gmail.com>
2026-08-24 23:53:01 +0500

    siimple Algorithm learned upto , solving fabonacci with loop and recursive

---
+++ simpleAlgorithm.py
@@ -0,0 +1,126 @@
+# ---------A Simple Algorithm-------
+# Fibonacci Numbers
+'''The Fibonacci numbers are very useful for introducing algorithms, so before we continue,
+ here is a short introduction to Fibonacci numbers.
+The Fibonacci numbers are named after a 13th century Italian mathematician known as Fibonacci.
+The two first Fibonacci numbers are 0 and 1, and the next Fibonacci number is always the sum of the two previous numbers, 
+so we get 0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+'''
+'''
+ let's implement three different versions of the algorithm to create Fibonacci numbers, just to see the difference between programming with loops and programming with recursion in a simple way.
+'''
+'''
+The Fibonacci Number Algorithm
+To generate a Fibonacci number, all we need to do is to add the two previous Fibonacci numbers.
+The Fibonacci numbers is a good way of demonstrating what an algorithm is. We know the principle of how to find the next number, so we can write an algorithm to create as many Fibonacci numbers as possible.
+Below is the algorithm to create the 20 first Fibonacci numbers.
+'''
+# How it works:
+'''
+Start with the two first Fibonacci numbers 0 and 1.
+Add the two previous numbers together to create a new Fibonacci number.
+Update the value of the two previous numbers.
+Do point a and b above 18 times.
+'''
+'''
+Loops vs Recursion
+To show the difference between loops and recursion, we will implement solutions to find Fibonacci numbers in three different ways:
+
+An implementation of the Fibonacci algorithm above using a for loop.
+An implementation of the Fibonacci algorithm above using recursion.
+Finding the 
+n
+th Fibonacci number using recursion.
+'''
+'''
+1. Implementation Using a For Loop
+It can be a good idea to list what the code must contain or do before programming it:
+
+Two variables to hold the previous two Fibonacci numbers
+A for loop that runs 18 times
+Create new Fibonacci numbers by adding the two previous ones
+Print the new Fibonacci number
+Update the variables that hold the previous two fibonacci numbers
+Using the list above, it is easier to write the program:
+'''
+# b = 1
+# a = 0
+# for fabo in range(18):
+#     newfebo = a + b
+#     print(newfebo)
+#     a = b
+#     b = newfebo
+'''
+the output is :
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+987
+1597
+2584
+4181
+'''
+'''
+2. Implementation Using Recursion
+Recursion is when a function calls itself.
+To implement the Fibonacci algorithm we need most of the same things as in the code example above,
+ but we need to replace the for loop with recursion.
+To replace the for loop with recursion, we need to encapsulate much of the code in a function, 
+and we need the function to call itself to create 
+a new Fibonacci number as long as the produced number of Fibonacci numbers is below, or equal to, 19.
+Our code looks like this:
+'''
+# print(0)
+# print(1)
+# count = 2 
+
+# def fabonacci(a , b):
+#     global count 
+#     if count <=19:
+#         newfabo = a + b 
+#         print(newfabo)
+#         a = b 
+#         b = newfabo
+#         count += 1
+#         fabonacci(a , b )
+#     else:
+#         return
+# fabonacci(0,1)
+'''
+output : 0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+987
+1597
+2584
+4181
+'''
+
+
+
